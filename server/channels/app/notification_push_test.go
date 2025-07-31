a7fd24e9d36870b463ed4b1139c4a0f334409ce1
kondo <85671197+kondo97@users.noreply.github.com>
2025-07-31 20:19:14 +0900

    Fix the issue where VoIP is not being used (#17) (#18)
    * fix: Update newSqlSessionStore to get VoipDeviceId
    
    * test: Add TestGetMobileAppSessions func test
    
    * chore: Fix lint style

--- server/channels/app/notification_push_test.go
+++ server/channels/app/notification_push_test.go
@@ -1773,3 +1773,24 @@ func BenchmarkPushNotificationThroughput(b *testing.B) {
 	b.StopTimer()
 	time.Sleep(2 * time.Second)
 }
+
+func TestGetMobileAppSessions(t *testing.T) {
+	t.Run("should return sessions that have a VoIP DeviceId value.", func(t *testing.T) {
+		th := Setup(t).InitBasic()
+		defer th.TearDown()
+
+		_, err := th.App.CreateSession(th.Context, &model.Session{
+			UserId:       th.BasicUser.Id,
+			DeviceId:     "DeviceId",
+			VoipDeviceId: "VoipDeviceId",
+			ExpiresAt:    model.GetMillis() + 100000,
+		})
+		require.Nil(t, err)
+
+		sessions, err := th.App.getMobileAppSessions(th.BasicUser.Id)
+		require.Nil(t, err)
+		require.Len(t, sessions, 1)
+		require.Equal(t, "DeviceId", sessions[0].DeviceId)
+		require.Equal(t, "VoipDeviceId", sessions[0].VoipDeviceId)
+	})
+}
